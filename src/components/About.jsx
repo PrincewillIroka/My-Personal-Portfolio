400421b0b915602a43453d0917d3cecca724cc02
PrincewillIroka <princewilliroka@outlook.com>
2020-01-13 18:41:44 +0100

    Updated About Page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import photo from '../assets/images/Twitter_Social_Icon_Circle_Color.png'
-import photo1 from '../assets/images/GitHub-Mark.png'
-import photo2 from '../assets/images/LI-In-Bug.png'
-import photo3 from '../assets/images/Instagram_AppIcon_Aug2017.png'
-import { connect } from 'react-redux'
-import { toggleActiveTab } from '../store/actions'
+import React, { Component } from "react";
+import styled from "styled-components";
+import photo from "../assets/images/Twitter_Social_Icon_Circle_Color.png";
+import photo1 from "../assets/images/GitHub-Mark.png";
+import photo2 from "../assets/images/LI-In-Bug.png";
+import photo3 from "../assets/images/Instagram_AppIcon_Aug2017.png";
+import { connect } from "react-redux";
+import { toggleActiveTab } from "../store/actions";
 
 class About extends Component {
   roundButtonClick(e, tab) {
-    this.props.toggleActiveTab(tab)
+    this.props.toggleActiveTab(tab);
   }
 
   downloadResume() {
-    const link = document.createElement('a')
-    link.href = './documents/Princewill_Iroka_Resume.pdf'
-    link.download = 'Princewill Iroka Resume'
-    link.dispatchEvent(new MouseEvent('click'))
+    const link = document.createElement("a");
+    link.href = "./documents/Princewill_Iroka_Resume.pdf";
+    link.download = "Princewill Iroka Resume";
+    link.dispatchEvent(new MouseEvent("click"));
   }
 
   render() {
@@ -26,55 +26,69 @@ class About extends Component {
           <span>Hello, I'm</span>
           <span>Princewill Iroka</span>
           <span>
-            It's my joy to see that the apps/software that I develop(or
-            collaborate in a team to develop) are solving/providing solutions to
-            real world problems for end-users.
+            I am a productive Software Developer who is passionate about
+            developing scalable, maintainable, cutting-edge web and mobile
+            apps. Using flexible modern Software Architecture principles, I can
+            work in a team or with minimal supervision to deliver high quality
+            software products that meets and exceeds users expectation.
           </span>
           <span>Find Me on</span>
           <div>
-            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/PrincewilIroka">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/PrincewilIroka"
+            >
               <img src={photo} alt="Twitter" />
             </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/PrincewillIroka">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/PrincewillIroka"
+            >
               <img src={photo1} alt="GitHub" />
             </a>
             <a
-              target="_blank" rel="noopener noreferrer"
+              target="_blank"
+              rel="noopener noreferrer"
               href="https://www.linkedin.com/in/princewill-iroka-19865784/"
             >
               <img src={photo2} alt="LinkedIn" />
             </a>
             <a
-              target="_blank" rel="noopener noreferrer"
+              target="_blank"
+              rel="noopener noreferrer"
               href="https://www.instagram.com/princewilliroka/"
             >
               <img src={photo3} alt="Instagram" />
             </a>
           </div>
-          <div className='c-t-div'>
-            <span className="hire-span" onClick={e => this.roundButtonClick(e, 'Contact')}>
+          <div className="c-t-div">
+            <span
+              className="hire-span"
+              onClick={e => this.roundButtonClick(e, "Contact")}
+            >
               HIRE ME
             </span>
-            <span className="resume-span" onClick={this.downloadResume}>MY RESUME</span>
+            <span className="resume-span" onClick={this.downloadResume}>
+              MY RESUME
+            </span>
           </div>
         </div>
       </Wrapper>
-    )
+    );
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
     toggleActiveTab: tab => {
-      dispatch(toggleActiveTab(tab))
+      dispatch(toggleActiveTab(tab));
     }
-  }
-}
+  };
+};
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(About)
+export default connect(null, mapDispatchToProps)(About);
 
 const Wrapper = styled.div`
   .aboutContainer {
@@ -85,25 +99,25 @@ const Wrapper = styled.div`
     > span:nth-child(1) {
       color: #707070;
       font-size: 17px;
-      font-family: 'Rubik', sans-serif;
+      font-family: "Rubik", sans-serif;
     }
 
     > span:nth-child(2) {
       color: #f44e91;
       font-weight: bold;
       font-size: 20px;
-      font-family: 'Roboto', sans-serif;
+      font-family: "Roboto", sans-serif;
     }
 
     > span:nth-child(3) {
       color: #707070;
-      font-family: 'Rubik', sans-serif;
+      font-family: "Rubik", sans-serif;
       font-size: 17px;
     }
 
     > span:nth-child(4) {
       color: #707070;
-      font-family: 'Fira Sans', sans-serif;
+      font-family: "Fira Sans", sans-serif;
       font-size: 18px;
     }
 
@@ -139,13 +153,13 @@ const Wrapper = styled.div`
       }
     }
 
-     .c-t-div {
-       display:flex;
+    .c-t-div {
+      display: flex;
       margin-top: 60px;
       > span {
         cursor: default;
       }
-       .hire-span {
+      .hire-span {
         background-color: #f33b86;
         border-radius: 50px;
         padding: 15px 40px;
@@ -160,7 +174,7 @@ const Wrapper = styled.div`
         }
       }
 
-       .resume-span {
+      .resume-span {
         border: 1px solid #f33b86;
         border-radius: 50px;
         padding: 15px 30px;
@@ -173,7 +187,6 @@ const Wrapper = styled.div`
         }
       }
     }
-
   }
 
   @media (max-width: 600px) {
@@ -184,13 +197,13 @@ const Wrapper = styled.div`
 
   @media (max-width: 785px) {
     .aboutContainer {
-      padding: 60px ;
+      padding: 60px;
     }
     .hire-span {
       padding: 15px 25px !important;
     }
-    .resume-span{
+    .resume-span {
       padding: 15px 20px !important;
     }
   }
-`
+`;
